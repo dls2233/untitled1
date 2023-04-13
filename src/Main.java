c498f7fa53490b3a04fb0d41984843c9c16f27ef
dls2233 <116965982+dls2233@users.noreply.github.com>
2023-04-13 21:41:09 +0800

    main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+class main{
+    public static void main(String[] args){
+        System.out.println("Hello,World!!你好中国！！");
+    }
+}
+class china{
+    public static void main(String[] args){
+
+    }
+}
